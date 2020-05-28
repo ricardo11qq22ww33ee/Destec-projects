35875a4802d897f47341e021dd988c92be64a428
Ivan12Xm <63081528+Ivan12Xm@users.noreply.github.com>
2020-05-28 15:18:55 -0700

    Update index.php

--- index.php
+++ index.php
@@ -91,6 +91,7 @@
         </div>
       </div>
     </header>
+    <hr class="mb-4">
     <div class="nav-scroller py-1 mb-2">
       <nav class="nav d-flex justify-content-between">
         <a class="p-2 text-muted" href="#">Algo</a>
